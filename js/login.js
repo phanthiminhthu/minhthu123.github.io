bf1776e1990255258dcaa6a887102e7d0e076df9
minhthu <minhthugiomy@gmail.com>
2020-01-09 21:39:41 +0700

    minhthu

---
+++ js/login.js
@@ -0,0 +1,15 @@
+function mydisplay() {
+    document.getElementById('image-flower').style.display='none';
+
+}
+// function shownImage(){
+// document.getElementById('image-flower').innerHTML=myFunction(4, 3);
+// }
+function show(){
+    window.alert (sum(4, 3));
+}
+function sum(p1, p2){
+    return p1 + p2;
+}
+function sum(){document.getElementById().innerText= ();
+}
